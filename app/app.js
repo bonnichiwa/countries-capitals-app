7b43ef7783aabecfdbe850d13cb43f6bdbc15ce7
bonnichiwa <bonnie.pham@ryerson.ca>
2015-09-26 23:52:31 -0400

    added routes, templates  and controller placeholders

--- app/app.js
+++ app/app.js
@@ -1,1 +1,25 @@
-angular.module('cncApp', ['ngRoute', 'ngAnimate'])
+angular.module('cncApp', ['ngRoute', 'ngAnimate', 'geolocation'])
+  .config(['$routeProvider', function($routeProvider){
+    $routeProvider.when('/', {
+        templateUrl : 'home.html',
+        controller : 'HomeCtrl'
+    })
+    .when('/countries', {
+      templateUrl : 'countries.html' ,
+      controller : 'CountryCtrl'
+    })
+    .when('/countries/:country/capital', {
+      templateUrl : 'capital.html',
+      controller : 'CapitalCtrl'
+    })
+  }])
+
+  .controller('HomeCtrl', function($scope) {
+      //empty for now
+  })
+  .controller('CountryCtrl', function($scope) {
+    //empty for now
+  })
+  .controller('CapitalCtrl', function($scope) {
+    //empty for now
+  });
---
+++ app/capital.html
@@ -0,0 +1,1 @@
+<h1>Capitals</h1>
